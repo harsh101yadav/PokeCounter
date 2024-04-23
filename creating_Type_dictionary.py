67360a6587b4f33390f47a4248ccb7d7ea6e872f
harsh101yadav <harsh101yadav@gmail.com>
2024-04-23 18:50:34 +0530

    trying nested for loop to help store data let us see

--- creating_Type_dictionary.py
+++ creating_Type_dictionary.py
@@ -1,23 +1,23 @@
 import type_match_up
-# import requests
-# from bs4 import BeautifulSoup
-# url = "https://pokemondb.net/type/dual"
-# r = requests.get(url)
-# soup = BeautifulSoup(r.text, 'html.parser')
+import requests
+from bs4 import BeautifulSoup
+url = "https://pokemondb.net/type/dual"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
 
 types = ["None","Normal","Fire","Water","Electric","Grass","Ice","Fighing","Poison","Ground","Flying","Psychic","Bug","Rock","Ghost","Dragon","Dark","Steel","FAiry"]
 
-#Creating list for all types
-# effectiveness_cells = soup.find_all("td", class_=lambda x: x and x.startswith("type-fx-cell"))
-# effectiveness_values = [cell.text.strip() for cell in effectiveness_cells]
+# Creating list for all types
+effectiveness_cells = soup.find_all("td", class_=lambda x: x and x.startswith("type-fx-cell"))
+effectiveness_values = [cell.text.strip() for cell in effectiveness_cells]
 
 
 #cearing lists within the list such that each type matchup has it's own list
-# chunk_size = 18
-# list_of_lists = []
-# for i in range(0, len(effectiveness_values), chunk_size):
-#     sublist = effectiveness_values[i:i+chunk_size]
-#     list_of_lists.append(sublist)
+chunk_size = 18
+list_of_lists = []
+for i in range(0, len(effectiveness_values), chunk_size):
+    sublist = effectiveness_values[i:i+chunk_size]
+    list_of_lists.append(sublist)
 
 
 #Opening python file and creating dictionaries
@@ -30,7 +30,10 @@ count = 0
 for i,type_name in enumerate(types,start = 1):
     x= "type_matchup_" + f"{type_name}"
     from type_match_up import x
-    for j in range(19):
-        if(j==0):
-            x.update({"":types})
+    x.update({"":types})
+    for j,secondary_type in enumerate(types):
+        if(type_name==secondary_type):
+            continue
+        x.update({[type_name,secondary_type]:list_of_lists[count]})
+        count = count + 1
         
